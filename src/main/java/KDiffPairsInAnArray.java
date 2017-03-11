a88a0480e0c0a40c902c8f8b3613b6dd00872147
Ruochen LI <ruochen.li87@gmail.com>
2017-03-11 18:34:10 +0100

    532. K-diff Pairs in an Array

--- src/main/java/KDiffPairsInAnArray.java
+++ src/main/java/KDiffPairsInAnArray.java
@@ -1,80 +1,34 @@
 import java.util.HashMap;
-import java.util.HashSet;
 import java.util.Map;
-import java.util.Set;
 
 /**
  * Created by Ruochen on 08/03/2017.
  */
 public class KDiffPairsInAnArray {
     public int findPairs(int[] nums, int k) {
-        int ans = 0;
-        Set<Pair> pairs = new HashSet<>();
-        for (int i = 0; i < nums.length; i++) {
-            for (int j = i + 1; j < nums.length; j++) {
-                Pair pair = new Pair(nums[i], nums[j]);
-                if (Math.abs(nums[i] - nums[j]) == k && !pairs.contains(pair)) {
-                    pairs.add(pair);
-                    ans++;
-                }
-            }
-        }
-        return ans;
-    }
 
-    private static final class Pair{
-        int a;
-        int b;
-        Pair(int a, int b){
-            this.a = Math.max(a, b);
-            this.b = a + b - this.a;
+        int result =0;
+        Map<Integer, Integer> occurrence = new HashMap<>();
+        for (int num : nums) {
+           occurrence.put(num, occurrence.getOrDefault(num, 0) + 1);
         }
 
-        @Override
-        public boolean equals(Object o) {
-            if (this == o) return true;
-            if (o == null || getClass() != o.getClass()) return false;
-
-            Pair pair = (Pair) o;
-
-            if (a != pair.a) return false;
-            return b == pair.b;
-
+        if (k < 0) {
+            return 0;
         }
 
-        @Override
-        public int hashCode() {
-            int result = a;
-            result = 31 * result + b;
-            return result;
-        }
-    }
-
-    /*
-    网友给出的O(n)的算法
-     */
-    public int findPairs2(int[] nums, int k) {
-        if (nums == null || nums.length == 0 || k < 0)   return 0;
-
-        Map<Integer, Integer> map = new HashMap<>();
-        int count = 0;
-        for (int i : nums) {
-            map.put(i, map.getOrDefault(i, 0) + 1);
-        }
-
-        for (Map.Entry<Integer, Integer> entry : map.entrySet()) {
+        for (Map.Entry<Integer, Integer> entry : occurrence.entrySet()) {
             if (k == 0) {
-                //count how many elements in the array that appear more than twice.
-                if (entry.getValue() >= 2) {
-                    count++;
-                }
-            } else {
-                if (map.containsKey(entry.getKey() + k)) {
-                    count++;
-                }
+                if (entry.getValue() > 1)
+                    result++;
+                continue;
+            }
+
+            if (occurrence.containsKey(entry.getKey() + k)) {
+                result++;
             }
         }
 
-        return count;
+        return result;
     }
 }
