1327355953b9aa8a795f06ed79440d0c5a10a888
Ruochen LI <ruochen.li87@gmail.com>
2017-03-25 21:02:29 +0100

    538. Convert BST to Greater Tree

--- src/main/java/common/TreeNode.java
+++ src/main/java/common/TreeNode.java
@@ -1,5 +1,7 @@
 package common;
 
+import static com.google.common.base.Objects.*;
+
 /**
  * Created by Ruochen on 19/03/2017.
  */
@@ -8,4 +10,21 @@ public class TreeNode {
     public TreeNode left;
     public TreeNode right;
     public TreeNode(int x) { val = x; }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        TreeNode that = (TreeNode) o;
+
+        return equal(this.val, that.val) &&
+                equal(this.left, that.left) &&
+                equal(this.right, that.right);
+    }
+
+    @Override
+    public int hashCode() {
+        return com.google.common.base.Objects.hashCode(val, left, right);
+    }
 }
